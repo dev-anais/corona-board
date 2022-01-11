8b453b1efa506a344e8f0a25b4d8da3b475c40b9
yuneunhye <38196875+yuneunhye@users.noreply.github.com>
2022-01-11 18:00:23 -0500

    made select / api connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import React, { useEffect, FC, useState } from "react";
 import styled from "styled-components";
+import axios, { AxiosResponse } from "axios";
+import { SummaryCovidResponse, SpecificInfo, Country } from "./types";
 import "./App.css";
+
+const SelectWrapper = styled.div`
+  min-height: 4vh;
+`;
+
+const CountriesSelect = styled.select`
+  width: 20vw;
+  float: right;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  background: #000000;
+  opacity: 0.9;
+  color: green;
+  padding: 8px;
+  font-weight: bold;
+  border-color: green;
+`;
 
 const TitlePanel = styled.div`
   width: 80vw;
@@ -7,11 +27,13 @@ const TitlePanel = styled.div`
   border-radius: 4px;
   border: 1px solid black;
   padding: 1rem;
-  background-color: rgba(0, 0, 0, 0.76);
+  background: #000000;
+  opacity: 0.9;
 `;
 
 const Title = styled.p`
   font-size: 30px;
+  font-weight: bold;
   color: white;
 `;
 
@@ -40,37 +62,115 @@ const SpecificInfoTotalNum = styled.p`
   font-size: 16px;
   color: white;
 `;
+const InfoText = styled.p`
+  font-size: 14px;
+  color: red;
+  align-self: center;
+`;
 
-function App() {
+// api
+function getSummaryCovidData(): Promise<AxiosResponse<SummaryCovidResponse>> {
+  const url = "https://api.covid19api.com/summary";
+  return axios.get(url);
+}
+
+const App: FC = () => {
+  async function initData() {
+    const { data } = await getSummaryCovidData();
+    /** TODO:
+     * 로컬 스토리지에 저장해둔 글로벌 정보가 있으면 해당 정보가 제일 먼저 보이도록,
+     * 만약 없으면 Global로 가는 것
+     *  */
+    setGlobalInfo(data.Global);
+    setCountriesInfo(data.Countries);
+  }
+
+  const [countriesInfo, setCountriesInfo] = useState<Array<Country>>();
+  const [globalInfo, setGlobalInfo] = useState<SpecificInfo>({
+    NewConfirmed: 0,
+    NewDeaths: 0,
+    NewRecovered: 0,
+    TotalConfirmed: 0,
+    TotalDeaths: 0,
+    TotalRecovered: 0,
+  });
+  const [selectedInfo, setSelectedInfo] = useState<SpecificInfo>();
+
+  useEffect(() => {
+    initData();
+  }, []);
+
+  const changeCountry = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    const selectedCountry = countriesInfo?.filter(
+      (country) => country.CountryCode === value
+    )[0];
+
+    if (selectedCountry) {
+      setSelectedInfo(selectedCountry);
+    }
+  };
   return (
     <div className="App">
       <TitlePanel>
+        <SelectWrapper>
+          <CountriesSelect onChange={changeCountry}>
+            <option></option>
+            {countriesInfo?.map((countryInfo, idx) => (
+              <option key={idx} value={countryInfo.CountryCode}>
+                {countryInfo.Country}
+              </option>
+            ))}
+          </CountriesSelect>
+        </SelectWrapper>
         <Title> 코로나바이러스감염증-19</Title>
         <MainInfoWrapper>
           <SpecificInfoWrapper>
-            <SpecificInfoTitle> 확진환자 </SpecificInfoTitle>
-            <SpecificInfoIncreaseNum>24</SpecificInfoIncreaseNum>
-            <SpecificInfoTotalNum>3000</SpecificInfoTotalNum>
+            <SpecificInfoTitle> 전세계 확진환자 </SpecificInfoTitle>
+            <SpecificInfoIncreaseNum>
+              {globalInfo.NewConfirmed}
+            </SpecificInfoIncreaseNum>
+            <SpecificInfoTotalNum>
+              {globalInfo.TotalConfirmed}
+            </SpecificInfoTotalNum>
           </SpecificInfoWrapper>
           <SpecificInfoWrapper>
-            <SpecificInfoTitle> 사망자 </SpecificInfoTitle>
-            <SpecificInfoIncreaseNum>24</SpecificInfoIncreaseNum>
-            <SpecificInfoTotalNum>3000</SpecificInfoTotalNum>
+            <SpecificInfoTitle> 전세계 사망자 </SpecificInfoTitle>
+            <SpecificInfoIncreaseNum>
+              {globalInfo.NewDeaths}
+            </SpecificInfoIncreaseNum>
+            <SpecificInfoTotalNum>
+              {globalInfo.TotalDeaths}
+            </SpecificInfoTotalNum>
           </SpecificInfoWrapper>
-          <SpecificInfoWrapper>
-            <SpecificInfoTitle> 격리해제 </SpecificInfoTitle>
-            <SpecificInfoIncreaseNum>24</SpecificInfoIncreaseNum>
-            <SpecificInfoTotalNum>3000</SpecificInfoTotalNum>
-          </SpecificInfoWrapper>
-          <SpecificInfoWrapper>
-            <SpecificInfoTitle> 치명률 </SpecificInfoTitle>
-            <SpecificInfoIncreaseNum>24</SpecificInfoIncreaseNum>
-            <SpecificInfoTotalNum>3000</SpecificInfoTotalNum>
-          </SpecificInfoWrapper>
+          {selectedInfo ? (
+            <>
+              <SpecificInfoWrapper>
+                <SpecificInfoTitle> 확진환자 </SpecificInfoTitle>
+                <SpecificInfoIncreaseNum>
+                  {selectedInfo.NewConfirmed}
+                </SpecificInfoIncreaseNum>
+                <SpecificInfoTotalNum>
+                  {selectedInfo.TotalConfirmed}
+                </SpecificInfoTotalNum>
+              </SpecificInfoWrapper>
+              <SpecificInfoWrapper>
+                <SpecificInfoTitle> 사망자 </SpecificInfoTitle>
+                <SpecificInfoIncreaseNum>
+                  {selectedInfo.NewDeaths}
+                </SpecificInfoIncreaseNum>
+                <SpecificInfoTotalNum>
+                  {selectedInfo.TotalDeaths}
+                </SpecificInfoTotalNum>
+              </SpecificInfoWrapper>
+            </>
+          ) : (
+            <InfoText> 상단에서 나라를 선택하여 주세요.</InfoText>
+          )}
         </MainInfoWrapper>
       </TitlePanel>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface SummaryCovidResponse {
+  Countries: Country[];
+  Date: string;
+  Global: SpecificInfo;
+  Message: string;
+}
+
+export interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+export interface SpecificInfo {
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
